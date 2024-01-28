05e6a403d53535ee6ef1906b3c1e35ffe5a86532
itayfroo <151866243+itayfroo@users.noreply.github.com>
2024-01-28 20:08:36 +0200

    j

---
+++ try.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import os
+with open("file.txt","w") as w:
+    inputfield = st.text_input("Text")
+    if st.button("launch"):
+        b = w.write(inputfield)
+with open("file.txt","r") as r:
+    st.write(r)
